fb22eace7fb6b527aadbcfe11ac91bcc74b73a33
Palash Bauri <palashbauri1@gmail.com>
2023-03-26 20:43:56 +0530

    added math's sqrt , log base e , log base 10 , log base X

--- cpank/stdlib/math.c
+++ cpank/stdlib/math.c
@@ -78,13 +78,68 @@ Value _math_lcm(int argc, Value* args) {
     return make_num(a * b / _get_gcd(a, b));
 }
 
+Value _math_sqrt(int argc, Value* args) {
+    if (argc != 1) {
+        return make_nil();
+    }
+
+    if (args[0].type != V_NUM) {
+        return make_nil();
+    }
+
+    double a = get_as_number(args[0]);
+    return make_num(sqrt(a));
+}
+
+Value _math_log10(int argc, Value* args) {
+    if (argc != 1) {
+        return make_nil();
+    }
+
+    if (args[0].type != V_NUM) {
+        return make_nil();
+    }
+    double a = get_as_number(args[0]);
+
+    return make_num(log10(a));
+}
+// Log base e
+Value _math_log(int argc, Value* args) {
+    if (argc != 1) {
+        return make_nil();
+    }
+
+    if (args[0].type != V_NUM) {
+        return make_nil();
+    }
+    double a = get_as_number(args[0]);
+
+    return make_num(log(a));
+}
+
+// Log base X
+Value _math_logx(int argc, Value* args) {
+    if (argc != 2) {
+        return make_nil();
+    }
+
+    if (args[0].type != args[1].type && args[0].type != V_NUM) {
+        return make_nil();
+    }
+
+    double base = get_as_number(args[0]);
+    double num = get_as_number(args[1]);
+
+    return make_num(log(num) / log(base));
+}
+
 void push_stdlib_math() {
     SL sls[] = {
-        msl(L"pow", _math_pow),
-        msl(L"add", _math_add),
-        msl(L"gcd", _math_gcd),
-        msl(L"lcm", _math_lcm),
+        msl(L"pow", _math_pow),   msl(L"add", _math_add),
+        msl(L"gcd", _math_gcd),   msl(L"lcm", _math_lcm),
+        msl(L"sqrt", _math_sqrt), msl(L"log10", _math_log10),
+        msl(L"log", _math_log),   msl(L"logx", _math_logx),
     };
 
-    _push_stdlib(L"math", sls, 4);
+    _push_stdlib(L"math", sls, 8);
 }
