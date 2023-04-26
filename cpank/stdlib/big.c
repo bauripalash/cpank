d15237f496b9302490701c9220960747c332f44f
Palash Bauri <palashbauri1@gmail.com>
2023-04-26 20:15:31 +0530

    added helper functions for basic arithmetic with objbignums

--- cpank/stdlib/big.c
+++ cpank/stdlib/big.c
@@ -1,8 +1,83 @@
 #include <gmp.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "../include/stdlib.h"
 #include "../include/vm.h"
+
+#define DO_ADD 20
+#define DO_SUB 21
+#define DO_MUL 22
+#define DO_DIV 23
+
+void do_float_calc(mpf_t result, mpf_t left, mpf_t right, uint8_t op) {
+    switch (op) {
+        case DO_ADD:
+            mpf_add(result, left, right);
+            break;
+        case DO_SUB:
+            mpf_sub(result, left, right);
+            break;
+        case DO_MUL:
+            mpf_mul(result, left, right);
+            break;
+        case DO_DIV:
+            mpf_div(result, left, right);
+            break;
+    }
+}
+
+void do_int_calc(mpz_t result, mpz_t left, mpz_t right, uint8_t op) {
+    switch (op) {
+        case DO_ADD:
+            mpz_add(result, left, right);
+            break;
+        case DO_SUB:
+            mpz_sub(result, left, right);
+            break;
+        case DO_MUL:
+            mpz_mul(result, left, right);
+            break;
+        case DO_DIV:
+            mpz_div(result, left, right);
+            break;
+    }
+}
+
+Value do_big_calc(PankVm *vm, ObjBigNum *left, ObjBigNum *right, uint8_t op) {
+    if (left->isfloat || right->isfloat) {
+        mpf_t f_left;
+        mpf_t f_right;
+
+        mpf_init(f_left);
+        mpf_init(f_right);
+
+        if (left->isfloat) {
+            mpf_set(f_left, left->as.fval);
+        } else {
+            mpf_set_z(f_right, left->as.ival);
+        }
+
+        if (right->isfloat) {
+            mpf_set(f_right, right->as.fval);
+        } else {
+            mpf_set_z(f_right, right->as.ival);
+        }
+        mpf_t result;
+        mpf_init(result);
+        do_float_calc(result, f_left, f_right, op);
+        Value v = make_obj_val(new_bignum_with_mpf(vm, result));
+        mpf_clear(result);
+        return v;
+    } else {
+        mpz_t result;
+        mpz_init(result);
+        do_int_calc(result, left->as.ival, right->as.ival, op);
+        Value v = make_obj_val(new_bignum_with_mpz(vm, result));
+        mpz_clear(result);
+        return v;
+    }
+}
 
 Value _big_add(PankVm *vm, int argc, Value *args) {
     check_argc_count("add", 2, argc);
@@ -13,40 +88,9 @@ Value _big_add(PankVm *vm, int argc, Value *args) {
             vm, U"both arguments to big.add(a,b) must be big numbers");
     }
 
-    ObjBigNum *big_a = get_as_bignum(raw_a);
-    ObjBigNum *big_b = get_as_bignum(raw_b);
-    if (big_a->isfloat || big_b->isfloat) {
-        mpf_t f_a;
-        mpf_t f_b;
-
-        mpf_init(f_a);
-        mpf_init(f_b);
-
-        if (big_a->isfloat) {
-            mpf_set(f_a, big_a->as.fval);
-        } else {
-            mpf_set_z(f_a, big_a->as.ival);
-        }
-
-        if (big_b->isfloat) {
-            mpf_set(f_b, big_b->as.fval);
-        } else {
-            mpf_set_z(f_b, big_b->as.ival);
-        }
-        mpf_t result;
-        mpf_init(result);
-        mpf_add(result, f_a, f_b);
-        Value v = make_obj_val(new_bignum_with_mpf(vm, result));
-        mpf_clear(result);
-        return v;
-    } else {
-        mpz_t result;
-        mpz_init(result);
-        mpz_add(result, big_a->as.ival, big_b->as.ival);
-        Value v = make_obj_val(new_bignum_with_mpz(vm, result));
-        mpz_clear(result);
-        return v;
-    }
+    ObjBigNum *left = get_as_bignum(raw_a);
+    ObjBigNum *right = get_as_bignum(raw_b);
+    return do_big_calc(vm, left, right, DO_ADD);
 }
 
 void push_stdlib_big(PankVm *vm) {
