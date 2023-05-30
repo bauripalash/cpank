c147010a098d591f7ef47868780c78f8c6a3aa26
Palash Bauri <palashbauri1@gmail.com>
2023-05-30 19:41:06 +0530

    added check to see if cli or gui symbolic links exists or not before creating links

--- build.zig
+++ build.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 const libiup = @import("cpank/ext/iup/build.zig");
 
+pub fn exists(filename : []const u8) !bool {
+    const f  = try std.fs.cwd().openFile(filename, .{});
+    
+    defer f.close();
+
+    _ = try f.stat();
+
+    return true;
+    
+    
+}
+
 pub fn build(b: *std.Build) void {
+    
+
     const cflags = [_][]const u8{ "-Wall", "-std=c11" };
 
     const sourceToRun = "sample/0.pank";
@@ -62,29 +76,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // the default cli pankti interpreter
     const exe = b.addExecutable(.{ .name = "pankti", .target = target, .optimize = optimize });
-    const lib = b.addStaticLibrary(.{ .name = "pankti", .optimize = optimize, .target = target });
-    const guiexe = b.addExecutable(.{ .name = "panktiw", .target = target, .optimize = optimize });
-    // if (target.isWindows()) {
-    //     guiFlags = [2][]const u8{
-    //         "-ldflags \"-H=windowsgui\"",
-    //         "-mwindows",
-    //     };
-    // } else {
-    //     guiFlags = [2][]const u8{ "", "" };
-    // }
-    guiexe.addCSourceFiles(&srcGui, &guiFlags);
-
     exe.linkLibC();
-    guiexe.linkLibC();
-    lib.linkLibC();
-
+    exe.linkSystemLibrary("m");
     exe.defineCMacro("MODE_BENGALI", "");
+    
+    // the static library of pankti interpreter with exposed `run_code(...)`
+    const lib = b.addStaticLibrary(.{ .name = "pankti", .optimize = optimize, .target = target });
+    lib.linkLibC();
     lib.defineCMacro("MODE_BENGALI", "");
     lib.defineCMacro("APILIB", "");
+
+
+    // the gui editor with pankti core integrated
+    const guiexe = b.addExecutable(.{ .name = "panktiw", .target = target, .optimize = optimize });
+    guiexe.linkLibC();
+    guiexe.addCSourceFiles(&srcGui, &guiFlags);
     guiexe.defineCMacro("MODE_BENGALI", "");
     guiexe.defineCMacro("APILIB", "lib");
-
+   
+    // The Iup Library
+    const ui = libiup.addIup(b, target, std.builtin.Mode.ReleaseFast);
+    
     //CORE
     exe.addCSourceFiles(&srcCore, &cflags);
     lib.addCSourceFiles(&srcCore, &cflags);
@@ -93,54 +107,45 @@ pub fn build(b: *std.Build) void {
     exe.addCSourceFiles(&srcStdlib, &cflags);
     lib.addCSourceFiles(&srcStdlib, &cflags);
 
-    //lib.addCSourceFiles(files: []const []const u8, flags: []const []const u8)
-
     //External
     exe.addCSourceFiles(&srcExt, &cflags);
     lib.addCSourceFiles(&srcExt, &cflags);
 
-    exe.linkSystemLibrary("m");
     guiexe.linkLibrary(lib);
-    const ui = libiup.addIup(b, target, std.builtin.Mode.ReleaseFast);
     guiexe.linkLibrary(ui);
 
+    // Add Icons and Version Information on Windows
     if (target.isWindows()) {
         exe.addObjectFile("cpankproj/cpankproj.res.obj");
         guiexe.addObjectFile("cpankproj/cpankproj.res.obj");
     }
 
+    // Prevents the gui variant to open a console
     guiexe.subsystem = .Windows;
 
- const create_exe_shortcut = b.addSystemCommand(&[_][]const u8{
+    //Create a symbolic link 
+    //Creates compatibility with existing Makefile based build steps
+    const create_exe_shortcut = b.addSystemCommand(&[_][]const u8{
         "ln",
         "-s",
         "zig-out/bin/pankti",
         "./"
     });
 
-    //Build Executable Command
-    var exe_build = b.step("x", "Build Executable 'pankti'");
-    const install_exe = b.addInstallArtifact(exe);
-    //exe.setOutputDir("build");
-    //Build GUI Executable
-    const gui_exe_build = b.step("g", "Build Gui 'panktiw'");
-    const install_gui_exe = b.addInstallArtifact(guiexe);
-    exe_build.dependOn(&install_exe.step);
-
-    exe_build.dependOn(&create_exe_shortcut.step);
-    gui_exe_build.dependOn(&install_gui_exe.step);
-
-    //Build Static Library
-    const lib_build = b.step("lib", "Build Static Library 'libpankti.a'");
-
-    const install_lib = b.addInstallArtifact(lib);
-    lib_build.dependOn(&install_lib.step);
-
+    const create_gui_shortcut = b.addSystemCommand(&[_][]const u8{
+        "ln",
+        "-s",
+        "zig-out/bin/panktiw",
+        "./"
+    });
+    
+    //Run The built cli aginst `sourceToRun`
     const run_cmd_run = b.addSystemCommand(&[_][]const u8{
         "zig-out/bin/pankti",
         sourceToRun,
     });
 
+    //Run python based unittest
     const test_command = b.addSystemCommand(&[_][]const u8{
         "python",
         "-m",
@@ -148,17 +153,41 @@ pub fn build(b: *std.Build) void {
         "-v",
     });
 
-   
+    //Build CLI Executable
+    var cli_build_step = b.step("x", "Build Executable 'pankti'");
+    const install_cli_exe = b.addInstallArtifact(exe);
+    cli_build_step.dependOn(&install_cli_exe.step);
 
-    const run_cmd = b.step("run", "Build and Run Sample");
-    run_cmd.dependOn(&exe.step);
-    run_cmd.dependOn(&run_cmd_run.step);
 
-    const test_cmd = b.step("test", "Run test using python");
-    test_cmd.dependOn(&exe.step);
-    test_cmd.dependOn(&test_command.step);
+    const cli_link_exists = exists("pankti") catch false;
+    if (target.isLinux() and !cli_link_exists) {
+        cli_build_step.dependOn(&create_exe_shortcut.step);
+    }
+
+    //Build GUI Executable
+    const gui_build_step = b.step("g", "Build Gui 'panktiw'");
+    const install_gui_exe = b.addInstallArtifact(guiexe);
+    gui_build_step.dependOn(&install_gui_exe.step);
+
+    const gui_link_exists = exists("panktiw") catch false;
+    if (target.isLinux() and !gui_link_exists) {
+        gui_build_step.dependOn(&create_gui_shortcut.step);
+    }
+
+    //Build API Static Library
+    const lib_build_step = b.step("lib", "Build Static Library 'libpankti.a'");
+    const install_api_lib = b.addInstallArtifact(lib);
+    lib_build_step.dependOn(&install_api_lib.step);
+
+    const run_code_step = b.step("run", "Build and Run Sample");
+    run_code_step.dependOn(&exe.step);
+    run_code_step.dependOn(&run_cmd_run.step);
+
+    const test_step = b.step("test", "Run test using python");
+    test_step.dependOn(&exe.step);
+    test_step.dependOn(&test_command.step);
     
-    b.default_step = exe_build;
+    b.default_step = cli_build_step;
     
 
     b.installArtifact(exe);
